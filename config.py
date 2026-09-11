4c734d5948024a70d63067168d563364e1f1249a
Sabarish <sabarish2385@gmail.com>
2026-09-11 22:17:26 +0530

    Prepare project for GitHub

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "2385",
-    "database": "garment_automation"
+    "host": os.getenv("DB_HOST", "localhost"),
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD", ""),
+    "database": os.getenv("DB_NAME", "garment_automation")
 }
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
